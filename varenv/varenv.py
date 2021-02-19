2aca8358a9fe799ad45c024b2b55375824999cb4
davincif <LDaVinciF@gmail.com>
2021-02-19 11:12:17 -0300

    feat: accepting variables with alias

--- varenv/varenv.py
+++ varenv/varenv.py
@@ -1,10 +1,9 @@
 import os
-
+import json
 try:
     import yaml
     hasyaml = True
 except Exception as exp:
-    import json
     hasyaml = False
 
 
@@ -35,6 +34,74 @@ def __detect_confile_extension():
     return [filetype, fileext]
 
 
+def __search_alias(varname: str, conf_dict):
+    alias_found = ''
+    value = None
+
+    if 'alias' not in conf_dict[varname]:
+        print("Varenv error: variable '%s' has no alias. Ignoring..." % varname)
+    elif 'value' not in conf_dict[varname]:
+        print("Varenv error: variable '%s' has no value. Ignoring..." % varname)
+    else:
+        for alias in conf_dict[varname]['alias']:
+            envval = os.getenv(alias)
+            if envval is not None:
+                # check of unsuported types
+                if not isinstance(conf_dict[varname]['value'], (int, float, complex, str, bool)):
+                    raise TypeError("%s, with value: %s. Is of type %s but only (int, float, complex, str, bool) are supported yet" % (
+                        varname, str(conf_dict[varname]['value']), type(conf_dict[varname]['value'])))
+
+                # set responses
+                alias_found = alias
+                value = envval
+                break
+
+    return [alias_found, value]
+
+
+def __get_var(varname: str, conf_dict):
+    value = None
+
+    typeof = type(conf_dict[varname])
+    # check of unsuported types
+    if not isinstance(conf_dict[varname], (int, float, complex, str, bool, dict)):
+        raise TypeError("%s, with value: %s. Is of type %s but only (int, float, complex, str, bool) are supported yet" % (
+            varname, str(conf_dict[varname]), type(conf_dict[varname])))
+
+    # check which variable and value is gonna be added, if any
+    if typeof is dict:
+        # check if object has the needed params
+        if 'alias' not in conf_dict[varname]:
+            varname = ''
+            print("Varenv error: variable '%s' has no alias. Ignoring..." % varname)
+        elif 'value' not in conf_dict[varname]:
+            varname = ''
+            print("Varenv error: variable '%s' has no value. Ignoring..." % varname)
+        else:
+            # search for the alias
+            varfound, valuefound = __search_alias(varname, conf_dict)
+            if not varfound:
+                value = conf_dict[varname]['value']
+            else:
+                varname = varfound
+                value = valuefound
+            typeof = type(value)
+    else:
+        # get var value, add on the os env and the envyVars obj
+        value = conf_dict[varname]
+
+    # now add value to varname if any was found
+    if varname:
+        value = value if typeof is str else str(value)
+        envval = os.getenv(varname)
+        if envval is None:
+            os.environ[varname] = value
+        else:
+            typeof = type(envval)
+
+    return [value, typeof]
+
+
 def __load_envy():
     global __CONF_FILE
     global envyVars
@@ -46,18 +113,18 @@ def __load_envy():
             __CONF_FILE = "./varenv.conf"
 
     # load local enviroment variables
-    conf_json = None
+    conf_dict = None
     try:
         filetype, fileexp = __detect_confile_extension()
         if filetype == 'yml' and hasyaml:
             print("ymal module detected")
             filename = __CONF_FILE + "." + fileexp
             with open(filename, "r") as confFile:
-                conf_json = yaml.load(confFile, Loader=yaml.FullLoader)
+                conf_dict = yaml.load(confFile, Loader=yaml.FullLoader)
         elif filetype == 'json':
             print("loading with json")
             with open(__CONF_FILE + ".json", "r") as confFile:
-                conf_json = json.loads(confFile.read())
+                conf_dict = json.loads(confFile.read())
         elif filetype == 'yml' and not hasyaml:
             print("Varenv warning: the file present is '%s.%s' but there's not yaml models" % (__CONF_FILE, fileexp))
         else:
@@ -66,21 +133,11 @@ def __load_envy():
         # nothing to be loaded
         print("Varenv warning: file '%s.(json, yml or yaml)' is not present, you are depending entirely on the already declared enviroment variables. If you are in produciton, ignore this warning." % __CONF_FILE)
     else:
-        for config in conf_json:
+        for config in conf_dict:
             # check of unsuported types
-            if not isinstance(conf_json[config], (int, float, complex, str, bool)):
-                raise TypeError("%s, with value: %s. Is of type %s but only (int, float, complex, str, bool) are supported yet" % (
-                    config, str(conf_json[config]), type(conf_json[config])))
-
-            typeof = type(conf_json[config])
-            value = conf_json[config] if typeof is str else str(conf_json[config])
-            envval = os.getenv(config)
-            if envval is None:
-                os.environ[config] = value
-            else:
-                value = envval
-                typeof = type(envval)
-            envyVars[config] = {'value': value, 'type': typeof}
+            value, typeof = __get_var(config, conf_dict)
+            if value is not None:
+                envyVars[config] = {'value': value, 'type': typeof}
 
 
 def __update_and_clean_envy():
